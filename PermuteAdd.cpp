ae0787234e1b4887b2545db395474d7c7d61a07a
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-08-20 00:35:52 -0400

    Doing permutations of numbers

---
+++ PermuteAdd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void Recursion(int *a, int *r)
+{
+  for (int i = 0; i < 4; i++)
+  {
+     
+  }
+}
+int main()
+{
+  int a[] = {1, 2, 3, 4};
+  int result[15] = {0};
+}
