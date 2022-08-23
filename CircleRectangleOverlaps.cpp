a17d934ddc8f0aced35a06d01844e785b14832f3
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-08-23 16:27:39 -0400

    Optimised solution for circle rectangle merging problem

--- CircleRectangleOverlaps.cpp
+++ CircleRectangleOverlaps.cpp
@@ -17,37 +17,23 @@ bool check(int x, int y, int r, int xC, int yC)
 }
 bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2)
 {
-  int l = abs(x2 - x1);
-  int w = abs(y2 - y1);
-  bool a = false;
   bool b = false;
   bool c = false;
-  bool d = false;
-  if (abs(x2 - xCenter) + abs(x1 - xCenter) == l && abs(y2 - yCenter) + abs(y1 - yCenter) == w)
-  {
-    d = true;
-  }
-  if (((abs(xCenter - x2) <= radius || abs(xCenter - x1) <= radius) && abs(y2 - yCenter) + abs(y1 - yCenter) == w) || ((abs(yCenter - y1) <= radius || abs(yCenter - y2) <= radius) && abs(x2 - xCenter) + abs(x1 - xCenter) == l))
+  if (((abs(xCenter - x2) + abs(xCenter - x1) <= 2 * radius + abs(x2 - x1)) && abs(y2 - yCenter) + abs(y1 - yCenter) == abs(y2 - y1)) || ((abs(yCenter - y1) + abs(yCenter - y2) <= 2 * radius + abs(y2 - y1)) && abs(x2 - xCenter) + abs(x1 - xCenter) == abs(x2 - x1)))
   {
     c = true;
-  }
-  if ((abs(x1 - xCenter) + abs(x2 - xCenter) == 2 * radius + l) && (abs(y1 - yCenter) + abs(y2 - yCenter) == 2 * radius + w))
-  {
-    a = true;
   }
   if ((check(x1, y1, radius, xCenter, yCenter) || check(x2, y2, radius, xCenter, yCenter) || check(x1, y2, radius, xCenter, yCenter) || check(x2, y1, radius, xCenter, yCenter)))
   {
     b = true;
   }
-  cout << "A is: " << a << endl;
   cout << "B is: " << b << endl;
   cout << "C is: " << c << endl;
-  cout << "D is: " << d << endl;
-  return a || b || c || d;
+  return b || c;
 }
 
 int main()
 {
 
-  cout << checkOverlap(2, 3, 1, 0, 6, 1, 10) << endl;
+  cout << checkOverlap(1, 1, 1, -3, -3, 3, 3) << endl;
 }
