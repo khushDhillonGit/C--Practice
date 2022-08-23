b28141184f21c39992a8b13e60816531be316a24
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-08-22 23:51:29 -0400

    Solved problem of representing a number by base 3

---
+++ PowersOfThree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+int power(int n)
+{
+  int x = 0;
+  while (n > 1)
+  {
+    n /= 3;
+    x++;
+  }
+  return x - 1;
+}
+int main()
+{
+  cout << power(243) << endl;
+}
