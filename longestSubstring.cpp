8d6f615220bba0fa4956f8900aaafe5a9a2185f6
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-08-29 15:19:20 -0400

    Found best solution for finding longest substring without duplicates

---
+++ longestSubstring.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+int recursion(string s, int start, int max)
+{
+  string k = "";
+  int count = 0;
+  if (start == s.length())
+  {
+    return max;
+  }
+  for (int i = start; i < s.length(); i++)
+  {
+    if (k.find(s[i]) > k.length())
+    {
+      k += s[i];
+      count++;
+    }
+    else
+    {
+      if (max < count)
+      {
+        max = count;
+      }
+      return recursion(s, start + 2, max);
+    }
+    if (max < count)
+    {
+      max = count;
+    }
+  }
+  return max;
+}
+int lengthOfLongestSubstring(string s)
+{
+  return recursion(s, 0, 0);
+}
+int main()
+{
+  string a = "pwwkew";
+  cout << lengthOfLongestSubstring(a);
+  // string k = "";
+  // k = a[7];
+  // cout << k << endl;
+}
