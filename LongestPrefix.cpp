ef8334cf1f51071c83136d9ba6b35a3aa90c410a
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-08-29 16:05:52 -0400

    program to get longest prefix from a string

---
+++ LongestPrefix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+string longestCommonPrefix(vector<string> &strs)
+{
+  string ans = "";
+  char a;
+  for (int j = 0; j < strs[0].length(); j++)
+  {
+    a = strs[0][j];
+    for (int i = 1; i < strs.size(); i++)
+    {
+      if (strs[i][j] != i)
+      {
+        return ans;
+      }
+    }
+    ans += a;
+  }
+  return ans;
+}
+int main()
+{
+}
