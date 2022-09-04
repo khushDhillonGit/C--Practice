341a93eaaf75dbf9a1f581f44e27b481297d83e1
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-09-03 20:08:28 -0400

    Created function to return element that is in majority in o(n) time and linear space

---
+++ CurMax.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+int majorityElement(vector<int> &nums)
+{
+  int major = nums[0], count = 1;
+  for (int i = 1; i < nums.size(); i++)
+  {
+    if (count == 0)
+    {
+      count++;
+      major = nums[i];
+    }
+    else if (major == nums[i])
+    {
+      count++;
+    }
+    else
+      count--;
+  }
+  return major;
+}
+int main()
+{
+  vector<int> a = {3, 2, 3};
+  cout << majorityElement(a);
+}
