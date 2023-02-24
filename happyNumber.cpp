3a7a55f5d03f18938d53774b485780e21b5d73d6
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2023-02-23 21:21:41 -0500

    program to find happy numbers

---
+++ happyNumber.cpp
@@ -0,0 +1,42 @@
+// Boiler plate for c++ competative
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int val)
+  {
+    val = val;
+    next = NULL;
+  }
+};
+int sumOfDigits(int a){
+    int total = 0;
+    while(a != 0){
+        total += pow((a%10),2);
+        a/=10;
+    }
+    return total;
+}
+bool isHappy(int n) {
+    vector<int> k;
+    k.push_back(n);
+    n = sumOfDigits(n);
+    while(n!=1){
+        if(find(k.begin(),k.end(),n) != k.end()){
+            return false;
+        }
+        n = sumOfDigits(n);
+        k.push_back(n);
+    }
+    return true;
+}
+int main()
+{
+    cout << isHappy(3) << endl;
+}
