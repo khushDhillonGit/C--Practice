6342f918b91ef03537a7d0b7166b7eeb57075dd3
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2023-04-11 22:10:21 -0400

    Solved question for finding min binary array equal to sum

---
+++ BinaryWithSum-930.cpp
@@ -0,0 +1,55 @@
+// Boiler plate for c++ competative
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int val)
+  {
+    val = val;
+    next = NULL;
+  }
+};
+
+       int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int i=0,j=0,count=0;
+        int prev=0;
+        bool done = false;
+        for(i,j;j<nums.size();j++){
+            goal-=nums[j];
+            while(goal <=0 && i<nums.size()){
+                if(goal==0){
+
+                count++;
+                }
+                if(nums[j+1]==0){
+                    prev = j;
+                    done = true;
+                    j++;
+                    while(nums[j]==0){
+                        count++;
+                        j++;
+                    }
+                }
+                if(done){
+                    j = prev+1;
+                    done = false;
+                }
+                goal+=nums[i++];
+            }
+
+        }
+        return count;
+    }
+
+int main()
+{
+    // vector<int> n{1,0,1,0,1};
+    vector<int> n{0,0,0,0,0,0,1,0,0,0};
+    cout<< numSubarraysWithSum(n,0) << endl;
+}
