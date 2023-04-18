cb040db6620ad21c9a16ad086f4fe3ce676b59a4
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-04-17 22:50:11 -0400

    Solution for inserting a new tuple without changing the order of list

---
+++ InsertIntervals57.cpp
@@ -0,0 +1,39 @@
+// Boiler plate for c++ competative
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int val)
+  {
+    val = val;
+    next = NULL;
+  }
+};
+
+vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+     vector<vector<int>> res;
+     intervals.insert(intervals.begin(),newInterval);
+    sort(intervals.begin(),intervals.end(),[](vector<int> a,vector<int> b){return a[0] < b[0];});
+    res.push_back(intervals[0]);
+    for(int i= 1;i<intervals.size();i++){
+        if(intervals[i][0] < res.back()[1]){
+            res.back()[1] = res.back()[1] > intervals[i][1] ? res.back()[1] : intervals[i][1];
+        }else{
+            res.push_back(intervals[i]);
+        }
+    }
+    return res;
+
+
+
+}
+int main()
+{
+
+}
