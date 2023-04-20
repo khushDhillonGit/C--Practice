b8f87b2dd8654a0ecb526fdb1bbc3f4429a69aa3
Khushwinder Singh <79319044+khushwindersingh1947@users.noreply.github.com>
2023-04-20 23:48:19 +0000

    Thu, Apr 20, 2023, 7:48 PM -04:00

--- EmployeeFreeTime.cpp
+++ EmployeeFreeTime.cpp
@@ -12,20 +12,35 @@ struct ListNode
 
 class Interval{
     public:
-        int start,int end;
+        int start,end;
         Interval(int _start, int _end){
             this->start = _start;
             this->end = _end;
         }
 }
-vector<Interval> empFreeTime(vector<vector<Interval>> &interval){
-    std:priority_queue<Interval,vector<Interval>,[](Interval start0,Interval start1){ return start0.start < start1.start; }>
-    for(vector<Interval> a: interval){
-        for(Interval)
+void empFreeTime(vector<vector<int>> &interval){
+
+    vector<Interval> res;
+
+    for(vector<int> a: interval){
+        for(int b: a){
+            res.push_back(b);
+        }
     }
+
+    std:priority_queue<Interval,vector<Interval>,[](Interval start0,Interval start1){ return start0.start < start1.start; }>
+    pq(res.begin(),res.end());
+
+    auto ans = pq.poll();
+    cout << ans.start << endl;
+    cout << ans.end << endl;
+
 
 }
 int main()
 {
+    vector<Interval> a {1,2,5,6};
+    vector<Interval> b {1,3};
+    vector<Interval> c {4,7};
 
 }
