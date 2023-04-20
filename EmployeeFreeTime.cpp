ad41989e60032e2dc033e053fbee83cd3407614a
Khushwinder Singh <79319044+khushwindersingh1947@users.noreply.github.com>
2023-04-20 23:29:06 +0000

    Thu, Apr 20, 2023, 7:29 PM -04:00

---
+++ EmployeeFreeTime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+};
+
+class Interval{
+    public:
+        int start,int end;
+        Interval(int _start, int _end){
+            this.start = _start;
+            this.end = _end;
+        }
+}
+
+vector<Interval> empFreeTime(vector<vector<Interval>> interval){
+
+
+}
+int main()
+{
+
+}
