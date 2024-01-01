cc80d4cb1bee68644478461033ae94031221de24
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2023-12-31 21:49:52 -0700

    last day of year 2023

---
+++ bitwiseTrailingzeros.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool hasTrailingZeros(vector<int> &nums)
+{
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if ((nums[i] | nums[j]) % 2 == 0)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+}
---
+++ longestspecialSubstr2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+  int maximumLength(string s) {
+        unordered_map<string, long long> map;
+        int res = -1;
+        long long i = 0;
+        for(long long j=0;j<=s.size();j++)
+        {
+            if(j==s.size() || s[j] != s[i])
+            {
+                long long c = j-i;
+            
+                for(long long k = 1;k<=c;k++)
+                {
+                    //if we already have till now largest possible length of substring we dont need to store the substrings
+                    //less than res length
+                    if(j-i > res)
+                    {
+                        map[s.substr(i,j-i)] += k;
+                        if(map[s.substr(i,j-i)] >= 3) res = j-i > res ? j-i :res;
+                    }
+                    i++;
+                }
+                i = j;
+            }
+        }
+        
+        return res;
+    }
+int main()
+{
+}
